4976fb7df1cc4e8fd6bb47a6e95468b87ca0ff1c
Alim Gunawan <dragoondance@gmail.com>
2025-02-04 14:53:26 +0700

    test

--- src/main/java/org/dragon/constant/Resources.java
+++ src/main/java/org/dragon/constant/Resources.java
@@ -1,19 +1,19 @@
 package org.dragon.constant;
 
 public class Resources {
-    public static String INVALID_NUMBER = "Invalid number format. example: 123.12";
-    public static String INVALID_COMMAND = "Invalid command, please use existing commands.";
-    public static String INVALID_SESSION = "Invalid session. please login to your account.";
-    public static String INVALID_AMOUNT = "Invalid amount supplied. please input amount correctly.";
-    public static String HELLO = "Hello, %s!";
-    public static String GOODBYE = "Goodbye, %s!";
-    public static String OWEDFROM = "Owed $%s from %s";
-    public static String OWEDTO = "Owed $%s to %s";
-    public static String TRANSFERREDTO = "Transferred $%s to %s";
-    public static String YOUR_BALANCE = "Your balance is: $%s";
-    public static String LIST_OF_COMMAND = "----List of command:----";
-    public static String LOGIN_COMMAND = "login [username] -> for login to account and access other menu";
-    public static String EXIT_COMMAND = "exit -> close MyBank apps";
+    public final static String INVALID_NUMBER = "Invalid number format. example: 123.12";
+    public final static String INVALID_COMMAND = "Invalid command, please use existing commands.";
+    public final static String INVALID_SESSION = "Invalid session. please login to your account.";
+    public final static String INVALID_AMOUNT = "Invalid amount supplied. please input amount correctly.";
+    public final static String HELLO = "Hello, %s!";
+    public final static String GOODBYE = "Goodbye, %s!";
+    public final static String OWEDFROM = "Owed $%s from %s";
+    public final static String OWEDTO = "Owed $%s to %s";
+    public final static String TRANSFERREDTO = "Transferred $%s to %s";
+    public final static String YOUR_BALANCE = "Your balance is: $%s";
+    public final static String LIST_OF_COMMAND = "----List of command:----";
+    public final static String LOGIN_COMMAND = "login [username] -> for login to account and access other menu";
+    public final static String EXIT_COMMAND = "exit -> close MyBank apps";
 
     public static String[] POST_LOGIN_COMMAND = new String[]{
         "deposit [amount] -> for deposit amount to active account's balance",
